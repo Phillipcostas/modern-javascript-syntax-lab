01ada96e01e5b17b85af644b341d292ae8fc580f
PhillipCostas <Phillipcostas@gmail.com>
2024-06-18 17:00:16 -0400

    completed assignment

---
+++ server.js
@@ -0,0 +1,141 @@
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+const numbers = nums.map(num => num * 2);
+// console.log(numbers)
+
+
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+// const [firstTopping, secondTopping] = pizzaToppings
+// console.log(firstTopping)
+// console.log(secondTopping)
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+const {make, model} = car
+const myCar = { ...car }
+myCar.model = 'q7';
+// console.log(car.make)
+// console.log(car.model)  
+console.log(myCar)
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const controversialPizzaToppings = [...pizzaToppings]
+// console.log(controversialPizzaToppings)
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+const userProfile = {
+    username: 'bob',
+    email: 'bob@aol.com',
+    age: 30,
+};
+const propertyName = 'username'
+const nameType = {
+    [propertyName]: 0,
+}
+
+// console.log(nameType)
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+const noun = 'cat';
+const adjective = 'white';
+
+// console.log(`the ${noun} is ${adjective}`)
+
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+if (pizza === 'tasty') {
+//   console.log('yum');
+} else {
+//   console.log('yuck');
+}
+
+// Your code here
+let tasty = 'yum' ? 'yes' : 'no';
+// console.log('tasty')
+
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+const lang = 'en' || localLangConfig
+
+
+
+// Log the result
+// console.log('Language setting:', lang);
+
+
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || 'light'
+
+
+// Log the result
+// console.log('User theme setting:', USER_THEME);
+
+
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+  let cat = adventurer.cat?.age;
+  console.log(cat);
+
+  
+
+
+
+
+
